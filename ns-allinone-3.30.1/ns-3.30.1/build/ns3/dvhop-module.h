bdab63f8b22e9876f4fe00d4fa4d136fe4e57203
19lamanna <amannalr@clarkson.edu>
2021-11-07 17:05:20 -0500

    add full ns3 folder with up-to-date dvhop

---
+++ ns-allinone-3.30.1/ns-3.30.1/build/ns3/dvhop-module.h
@@ -0,0 +1,14 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_DVHOP
+    
+
+// Module headers:
+#include "distance-table.h"
+#include "dvhop-helper.h"
+#include "dvhop-packet.h"
+#include "dvhop.h"
+#endif
